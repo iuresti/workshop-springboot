e9e51e18f7fe51e63ac77842d2366fc5c0e76b45
iuresti <iuresti@skytouchtechnology.com>
2019-03-15 01:25:06 -0600

    Add entities

---
+++ src/main/java/org/desarrolladorslp/workshops/springboot/models/Column.java
@@ -0,0 +1,54 @@
+package org.desarrolladorslp.workshops.springboot.models;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "columns",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"board_id", "name"})
+        })
+public class Column {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @javax.persistence.Column(length = 30)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+}
