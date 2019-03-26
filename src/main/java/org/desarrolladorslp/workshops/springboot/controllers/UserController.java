dc8e098410a4c608cdba5389aadc5ac17370a16d
iuresti <iuresti@skytouchtechnology.com>
2019-03-26 17:14:11 -0600

    use XXXMapping annotations instead of RequestMapping

--- src/main/java/org/desarrolladorslp/workshops/springboot/controllers/UserController.java
+++ src/main/java/org/desarrolladorslp/workshops/springboot/controllers/UserController.java
@@ -1,63 +1,60 @@
 package org.desarrolladorslp.workshops.springboot.controllers;
 
+import java.util.List;
+
 import org.desarrolladorslp.workshops.springboot.models.User;
 import org.desarrolladorslp.workshops.springboot.services.UserService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
     private UserService userService;
 
-    public UserController(UserService userService){
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @RequestMapping(value = "/user", method = RequestMethod.POST)
-    @ResponseBody
-    private HttpStatus createUser(@RequestParam(value = "email", required = true) String email, @RequestParam(value = "name", required = true) String name) {
-        User user = new User();
-        user.setEmail(email);
-        user.setName(name);
-        userService.saveUser(user);
-        return HttpStatus.OK;
+    @PostMapping(value = "/user")
+    private ResponseEntity<User> createUser(@RequestBody User user) {
+        return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
     }
 
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
-    @ResponseBody
-    private User getUserByEmail(@RequestParam(value="email", required = true)String email) {
-        return userService.findByEmail(email);
+    @GetMapping(value = "/user")
+    private ResponseEntity<User> getUserByEmail(@RequestParam(value = "email", required = true) String email) {
+        return new ResponseEntity<>(userService.findByEmail(email), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    private User getUsersById(@PathVariable("id")Long id) throws Exception {
-        return userService.findById(id);
+    @GetMapping(value = "/user/{id}")
+    private ResponseEntity<User> getUsersById(@PathVariable("id") Long id) throws Exception {
+        return new ResponseEntity<>(userService.findById(id), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
-    @ResponseBody
-    private HttpStatus deleteUser(@PathVariable("id")Long id) throws Exception {
+    @DeleteMapping(value = "/user/{id}")
+    private ResponseEntity deleteUser(@PathVariable("id") Long id) throws Exception {
+
         userService.deleteUser(id);
-        return HttpStatus.OK;
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/user/{id}", method = RequestMethod.PATCH)
-    @ResponseBody
-    private HttpStatus updateUser(@PathVariable("id")Long id,@RequestParam(value = "email", required = true) String email, @RequestParam(value = "name", required = true) String name) throws Exception {
-        User user = userService.findById(id);
-        user.setName(name);
-        user.setEmail(email);
-        userService.updateUser(user);
-        return HttpStatus.OK;
+    @PatchMapping(value = "/user/{id}")
+    private ResponseEntity<User> updateUser(@RequestBody User user) throws Exception {
+        return new ResponseEntity<>(userService.updateUser(user), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/users", method = RequestMethod.GET)
-    @ResponseBody
-    private List<User> getAllUsers() {
-        return userService.allUsers();
+    @GetMapping(value = "/users")
+    private ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.allUsers(), HttpStatus.OK);
     }
 }
