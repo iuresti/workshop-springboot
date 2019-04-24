8c86a2f5b40ca9870e3c2ae2e298ca293e25d0f4
Miguel Ramirez <rooqter@hotmail.com>
2019-04-24 14:34:45 -0500

    Added UserDetailsServiceImpl, exceptions package, RegistrationForm. Updated User Service/ServiceImpl/Respository. Updated ErrorHandler.

---
+++ src/main/java/org/desarrolladorslp/workshops/springboot/security/CustomUserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package org.desarrolladorslp.workshops.springboot.security;
+
+import org.desarrolladorslp.workshops.springboot.models.User;
+import org.desarrolladorslp.workshops.springboot.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        if(!user.isPresent()) { throw new UsernameNotFoundException(username); }
+        return user.get();
+    }
+}
--- src/main/java/org/desarrolladorslp/workshops/springboot/services/impl/UserServiceImpl.java
+++ src/main/java/org/desarrolladorslp/workshops/springboot/services/impl/UserServiceImpl.java
@@ -1,21 +1,37 @@
 package org.desarrolladorslp.workshops.springboot.services.impl;
 
-import java.util.List;
-
-import javax.persistence.EntityNotFoundException;
-
+import org.desarrolladorslp.workshops.springboot.exceptions.EmailAlreadyRegisteredException;
+import org.desarrolladorslp.workshops.springboot.exceptions.InternalServerException;
+import org.desarrolladorslp.workshops.springboot.exceptions.UsernameAlreadyRegisteredException;
+import org.desarrolladorslp.workshops.springboot.forms.RegistrationForm;
+import org.desarrolladorslp.workshops.springboot.models.Role;
 import org.desarrolladorslp.workshops.springboot.models.User;
+import org.desarrolladorslp.workshops.springboot.models.RoleName;
+import org.desarrolladorslp.workshops.springboot.repository.RoleRepository;
 import org.desarrolladorslp.workshops.springboot.repository.UserRepository;
 import org.desarrolladorslp.workshops.springboot.services.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository,
+                           RoleRepository roleRepository,
+                           PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -34,6 +50,11 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow(() -> new EntityNotFoundException("Username not found"));
+    }
+
+    @Override
     public User findById(Long id) throws EntityNotFoundException {
         return userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("User not found"));
     }
@@ -47,5 +68,48 @@ public class UserServiceImpl implements UserService {
     @Override
     public User updateUser(User user) {
         return userRepository.save(user);
+    }
+
+    @Override
+    public User createUser(RegistrationForm registrationForm) {
+
+        if(Objects.isNull(registrationForm) || Objects.isNull(registrationForm.getEmail()) ||
+                Objects.isNull(registrationForm.getUsername()) || Objects.isNull(registrationForm.getName())
+                || Objects.isNull(registrationForm.getPassword())) {
+            throw new IllegalArgumentException("registrationForm must not be null or contain empty values");
+        }
+
+        // Check if the given email has already been registered.
+        userRepository.findByEmail(registrationForm.getEmail().toLowerCase())
+                .ifPresent(user -> {
+            throw new EmailAlreadyRegisteredException(
+                    "Email " + registrationForm.getEmail() + " has already been registered");
+        });
+
+        // Check if the given username has already been registered.
+        userRepository.findByUsername(registrationForm.getUsername().toLowerCase())
+                .ifPresent(user -> {
+                    throw new UsernameAlreadyRegisteredException(
+                            "Username " + registrationForm.getEmail() + " has already been registered");
+                });
+
+        final Set<Role> rolesList = new HashSet<>();
+        roleRepository.findByName(RoleName.ROLE_USER).ifPresent(rolesList::add);
+
+        if(rolesList.isEmpty()) {
+            throw new InternalServerException("An error has occurred while setting new user roles");
+        }
+
+        final User newUser = User.builder()
+                .email(registrationForm.getEmail().toLowerCase())
+                .username(registrationForm.getUsername().toLowerCase())
+                .name(registrationForm.getName().toLowerCase())
+                .password(passwordEncoder.encode(registrationForm.getPassword()))
+                .enabled(true)
+                .authorities(rolesList)
+                .build();
+
+        return userRepository.save(newUser);
+
     }
 }
